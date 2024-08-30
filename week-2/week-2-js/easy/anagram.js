247f803b561a356faca740f7743e6e18406ae554
Gokilp <gokilp25@gmail.com>
2024-08-30 16:08:42 +0530

    complted Angram

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -4,8 +4,25 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
-function isAnagram(str1, str2) {
+// lower case 
+// String to Array 
 
+function isAnagram(str1, str2) {
+  // Check if the lengths of the strings are equal
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  // Convert strings to lowercase, split into characters, sort, and join back to a string
+  const sortedStr1 = str1.toLowerCase().split('').sort().join('');
+  const sortedStr2 = str2.toLowerCase().split('').sort().join('');
+
+  // Return whether the sorted strings are equal
+  return sortedStr1 === sortedStr2;
 }
 
+// Example usage
+console.log(isAnagram("aabbacc", "aaabbacc")); // false
+
+// Export the function
 module.exports = isAnagram;
