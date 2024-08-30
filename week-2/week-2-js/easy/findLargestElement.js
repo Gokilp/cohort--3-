706ae344c1286131acb1f48ba58d79c661fe97bd
Gokilp <gokilp25@gmail.com>
2024-08-30 16:25:01 +0530

    complted largestNumber

--- week-2/week-2-js/easy/findLargestElement.js
+++ week-2/week-2-js/easy/findLargestElement.js
@@ -7,6 +7,15 @@
 
 function findLargestElement(numbers) {
     
+  let largeset = numbers[0];
+  
+  for(let i=1;i<numbers.length;i++)
+  {
+     if(numbers[i]>largeset)
+        largeset = numbers[i];
+  }
+  return largeset;
 }
-
+let arr = [3,7,2,9,1];
+findLargestElement(arr)
 module.exports = findLargestElement;
